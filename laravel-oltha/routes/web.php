b7eb1267bf68ec5932dff9b8dc661e9d37569a77
olthapage <olthar.rosyeda01@gmail.com>
2025-02-24 20:32:16 +0700

    Praktikum Routing

--- laravel-oltha/routes/web.php
+++ laravel-oltha/routes/web.php
@@ -13,6 +13,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Basic Routing
+// Routing di Laravel membutuhkan informasi mengenai http verb kemudian input berupa url dan apa yang harus dilakukan ketika menerima url tersebut
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2341720145';
+});
+
+// Route Parameters
+// Terkadang saat membuat sebuah URL, kita perlu mengambil sebuah parameter yang merupakan bagian dari segmen URL dalam route kita
+Route::get('/user/{oltha}', function ($oltha) {
+    return 'Nama saya'. $oltha;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID'.$id;
+});
+
+// Optional Parameters (percobaan pertama $name=null, lalu percobaan kedua diubah menjadi $name='John')
+// Kita dapat menentukan nilai parameter route, tetapi menjadikan nilai parameter route tersebut opsional
+Route::get('/user/{name?}', function ($name='John')  {
+    return 'Nama saya '.$name; 
+});
+
