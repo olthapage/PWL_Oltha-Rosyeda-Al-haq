e2429405fc17b73abfb44f411e37d075c9bebb23
olthapage <olthar.rosyeda01@gmail.com>
2025-03-10 18:53:15 +0700

    praktikum 2.1_Jobsheet 4_PWL

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -9,7 +9,8 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller {
     public function index() {
 
-        $data = [
+        // praktikum 1
+        /*$data = [
             'level_id' => 2,
             'username' => 'manager_tiga',
             'nama' => 'Manager 3',
@@ -18,7 +19,22 @@ class UserController extends Controller {
         UserModel::insert($data);
         
         $user = UserModel::all(); 
+        return view('user', ['data' => $user]);*/
+
+        // praktikum 2.1
+        /*$user = UserModel::firstWhere('level_id', 1);
         return view('user', ['data' => $user]);
+
+        $user = UserModel::findOr(1, ['username', 'nama'], function () {
+            abort(404);
+        });
+        return view('user', ['data' => $user]);*/
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
+        
 
 
     }
