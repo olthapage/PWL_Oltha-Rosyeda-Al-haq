7f4bb3e08a452c0357e84cace07a49ec4a855af3
olthapage <olthar.rosyeda01@gmail.com>
2025-03-10 17:13:57 +0700

    Jobsheet 4_PWL

---
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller {
+    
+}
