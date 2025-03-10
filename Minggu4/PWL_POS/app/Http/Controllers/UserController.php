d30682e0717f68e505eac676bfb6df7319f8f02b
olthapage <olthar.rosyeda01@gmail.com>
2025-03-10 17:27:04 +0700

    Praktikum 1_Minggu 4_PWL

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -7,5 +7,19 @@ use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller {
-    
+    public function index() {
+
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::insert($data);
+        
+        $user = UserModel::all(); 
+        return view('user', ['data' => $user]);
+
+
+    }
 }
