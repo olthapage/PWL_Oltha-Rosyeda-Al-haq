0be7791e03793c6e603bc1d29a26790211429026
olthapage <olthar.rosyeda01@gmail.com>
2025-03-11 18:27:32 +0700

    praktikum 2.3_jobsheet4_PWL

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -37,10 +37,20 @@ class UserController extends Controller {
 
         // praktikum 2.2
         /*$user = UserModel::findOrFail(1);
+        return view('user', ['data' => $user]);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);*/
 
-        $user = UserModel::where('username'. 'manager9')->firstOrFail();
+        // praktikum 2.3
+        /*$user = UserModel::where('level_id', 2)->count();
+        dd($user);
+        return view('user', ['data' => $user]);*/
+
+        $user = UserModel::where('level_id', 2)->count();
         return view('user', ['data' => $user]);
+
+
 
 
         
