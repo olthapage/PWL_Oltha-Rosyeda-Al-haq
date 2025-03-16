b9ea35f71617bbd72c29d4e6275994de7a997eef
olthapage <olthar.rosyeda01@gmail.com>
2025-03-16 10:09:41 +0700

    praktikum1_jobsheet5_PWL

--- Minggu5/PWL_POS/resources/views/welcome.blade.php
+++ Minggu5/PWL_POS/resources/views/welcome.blade.php
@@ -1,12 +1,26 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PWL 2024</title>
-    </head>
-    <body>
-        <h2>Pemrograman Web Lanjut 2024</h2>
-        <h3>Semangat Belajar Laravel 10</h3>
-    </body>
-</html>
+@extends('layouts.app')
+
+{{-- Customize layout sections --}}
+
+@section('subtitle', 'Welcoe')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Welcome')
+
+{{-- Content body: main page content --}}
+
+@section('content_body')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+{{-- Push extra CSS --}}
+
+@push('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@endpush
+
+{{-- Push extra scripts --}}
+
+@push('js')
+    <script> console.log("Hi, I,m usingthe laravel-AdminLTE package!")</script>
+@endpush
