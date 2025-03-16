a131a379dad32d3a526630f3e3d53413ff6a4e5a
olthapage <olthar.rosyeda01@gmail.com>
2025-03-16 09:39:02 +0700

    Jobsheet 5

---
+++ Minggu5/PWL_POS/resources/views/About.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Percobaan PWL</title>
+    </head>
+    <body>
+        <h2>Pemrograman Web Lanjut 2024</h2>
+        <h3>About</h3>
+    </body>
+</html>
