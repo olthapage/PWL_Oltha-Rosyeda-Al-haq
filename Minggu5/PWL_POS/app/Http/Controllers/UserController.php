6cbda452750a5c236b39cdb2acac46905a156cad
olthapage <olthar.rosyeda01@gmail.com>
2025-03-24 13:53:36 +0800

    praktikum4_jobsheet5_PWL

--- Minggu5/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu5/PWL_POS/app/Http/Controllers/UserController.php
@@ -177,8 +177,9 @@ class UserController extends Controller
         ];
 
         $activeMenu = 'user'; // set menu yang sedang aktif
+        $level = LevelModel::all();
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level,'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datatables
@@ -186,6 +187,11 @@ class UserController extends Controller
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users -> where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
