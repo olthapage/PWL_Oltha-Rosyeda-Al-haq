71c7e1b2533775b06fe6ddc5e70f43a6438c1212
olthapage <olthar.rosyeda01@gmail.com>
2025-03-26 14:56:28 +0800

    praktikum3_jobsheet6

--- Minggu6/PWL_POS/routes/web.php
+++ Minggu6/PWL_POS/routes/web.php
@@ -51,6 +51,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // Menampilkan halaman form edit user Ajax
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // Menyimpan perubahan data user Ajax
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); // untuk tampilkan form delete user ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // hapus data user ajax
 });
 
 Route::group(['prefix' => 'level'], function () {
