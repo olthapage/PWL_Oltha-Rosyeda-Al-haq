386639d958c3605ed81c3c6510398538e94d9f48
olthapage <olthar.rosyeda01@gmail.com>
2025-04-03 22:15:23 +0800

    tugas praktikum 1 _implementasi logout

--- Minggu7/PWL_POS/resources/views/layouts/sidebar.blade.php
+++ Minggu7/PWL_POS/resources/views/layouts/sidebar.blade.php
@@ -64,6 +64,17 @@
             <p>Data Supplier</p>
         </a>
       </li>
+      <li class="nav-header">Keluar</li>
+      <li class="nav-item">
+          <a href="#" class="nav-link"
+              onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+              <i class="nav-icon fas fa-sign-out-alt"></i>
+              <p>Logout</p>
+          </a>
+          <form id="logout-form" action="{{ url('logout') }}" method="POST" style="display: none;">
+              @csrf
+          </form>
+      </li>
     
     </ul>
   </nav>
