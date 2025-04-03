f2486d23f62863de9c9438aa851690c7862dd0ef
olthapage <olthar.rosyeda01@gmail.com>
2025-04-03 22:37:25 +0800

    praktikum 2 _jobsheet7

--- Minggu7/PWL_POS/routes/web.php
+++ Minggu7/PWL_POS/routes/web.php
@@ -146,4 +146,22 @@ Route::middleware(['auth'])->group(function() { // artinya semua route di dalam 
 
     Route::get('/', [WelcomeController::class, 'index']);
     // masukkan semua route yang perlu autentikasi di sini
+    Route::middleware(['authorize:ADM'])->group(function(){
+        Route::group(['prefix' => 'level'], function () {
+            Route::get('/', [LevelController::class, 'index']); 
+            Route::post('/list', [LevelController::class, 'list']); 
+            Route::get('/create', [LevelController::class, 'create']); 
+            Route::post('/', [LevelController::class, 'store']); 
+            Route::get('/create_ajax', [LevelController::class, 'create_ajax']);  
+            Route::post('/ajax', [LevelController::class, 'store_ajax']); 
+            Route::get('/{id}', [LevelController::class, 'show']);
+            Route::get('/{id}/edit', [LevelController::class, 'edit']); 
+            Route::put('/{id}', [LevelController::class, 'update']);
+            Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']); 
+            Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']); 
+            Route::delete('/{id}', [LevelController::class, 'destroy']); 
+            Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']); 
+            Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']); 
+        });
+    });
 });
