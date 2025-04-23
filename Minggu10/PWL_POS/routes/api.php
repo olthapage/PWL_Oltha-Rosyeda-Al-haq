447cc6ece98501709b89d01dc2097897b14224ef
olthapage <olthar.rosyeda01@gmail.com>
2025-04-23 18:33:05 +0700

    praktikum 2 dan 3_jobsheet 10_pwl

--- Minggu10/PWL_POS/routes/api.php
+++ Minggu10/PWL_POS/routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
